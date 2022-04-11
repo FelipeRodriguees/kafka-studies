d38dd687f35bce42af976257bd6b5167883aaa92
Felipe Rodrigues <felipe@192.168.1.9>
2022-04-10 21:15:44 -0300

    Code annotations added.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kafka-producer-studie
 Message producer API for my studies in Kafka.
+
+I am studying using the Alura platform.
--- src/main/java/com/example/ecommerce/NewOrder.java
+++ src/main/java/com/example/ecommerce/NewOrder.java
@@ -10,10 +10,10 @@ import java.util.concurrent.ExecutionException;
 
 public class NewOrder {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "2312, 12312, 123213";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        var producer = new KafkaProducer<String, String>(properties()); // Instance a new Kafka producer and with your key and message type. The producer instance wait any properties.
+        var value = "2312, 12312, 123213"; // Static message.
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value); // Instance a new Producer Recorde and set one topic, key and value. I'm lost on what's the key.
+        producer.send(record, (data, ex) -> { // Error handling.
             if (ex != null) {
                 ex.printStackTrace();
                 return;
@@ -24,9 +24,9 @@ public class NewOrder {
 
     private static Properties properties() {
         var proprieties = new Properties();
-        proprieties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        proprieties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        proprieties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        proprieties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092"); // Set ip and port the kafka server.
+        proprieties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializer String for bytes.
+        proprieties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializer String for bytes.
         return proprieties;
     }
 }
